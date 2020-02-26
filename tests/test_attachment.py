8146ce275d747c00c1b169cd5ce20feed4b4b898
veoco <one@nomox.cn>
2020-02-27 00:48:49 +0800

    add more tests

--- tests/test_attachment.py
+++ tests/test_attachment.py
@@ -1,3 +1,24 @@
+from pytypecho import Attachment
+
+
 def test_get_attachments(te):
-    r = te.get_attachments()
-    assert r == []
+    with open('resources/test.png', 'rb') as f:
+        data = Attachment('test.png', f.read())
+        te.new_attachment(data)
+        r = te.get_attachments()
+        assert r
+
+
+def test_get_attachment(te):
+    with open('resources/test.png', 'rb') as f:
+        data = Attachment('test.png', f.read())
+        te.new_attachment(data)
+        r = te.get_attachment(3)
+        assert r
+
+
+def test_new_attachment(te):
+    with open('resources/test.png', 'rb') as f:
+        data = Attachment('test.png', f.read())
+        r = te.new_attachment(data)
+        assert r['file'] == 'test.png'
