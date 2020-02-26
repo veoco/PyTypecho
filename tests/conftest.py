0a3e8117f672f768d54053f0893fc77ef34eb948
veoco <one@nomox.cn>
2020-02-27 01:12:18 +0800

    remove attachment tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,4 +5,5 @@ from pytypecho import Typecho
 
 @pytest.fixture(scope="module")
 def te():
-    return Typecho('http://127.0.0.1:4567/index.php/action/xmlrpc', username='admin', password='admin')
+    return Typecho('http://192.168.50.70:8080/index.php/action/xmlrpc', username='admin', password='admin')
+    # return Typecho('http://127.0.0.1:4567/index.php/action/xmlrpc', username='admin', password='admin')
--- tests/test_attachment.py
+++ tests/test_attachment.py
@@ -1,24 +1,3 @@
-from pytypecho import Attachment
-
-
 def test_get_attachments(te):
-    with open('test.png', 'rb') as f:
-        data = Attachment('test.png', f.read())
-        te.new_attachment(data)
-        r = te.get_attachments()
-        assert r
-
-
-def test_get_attachment(te):
-    with open('test.png', 'rb') as f:
-        data = Attachment('test.png', f.read())
-        te.new_attachment(data)
-        r = te.get_attachment(3)
-        assert r
-
-
-def test_new_attachment(te):
-    with open('test.png', 'rb') as f:
-        data = Attachment('test.png', f.read())
-        r = te.new_attachment(data)
-        assert r['file'] == 'test.png'
+    r = te.get_attachments()
+    assert r == []
