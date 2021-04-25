0d080426dd6579250d0619a2481c235c8c3e54b8
Veoco <veoco@users.noreply.github.com>
2021-04-25 15:20:37 +0800

    Develop (#6)
    * add docs
    
    * change api and add models.py
    
    * add more tests
    
    * update README.md
    
    * update python version requirement
    
    * change test.png position
    
    * remove attachment tests
    
    * fix url
    
    * change python version
    
    * fix sphinx config
    
    * update README.md
    
    * update version to 2.0
    
    * update README.md
    
    * fix #5
    
    * update version to 2.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pytypecho",
-    version="2.0.0",
+    version="2.0.1",
     author="Veoco",
     author_email="one@nomox.cn",
     description="Python Typecho Client (XMLRPC)",
