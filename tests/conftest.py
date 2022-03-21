1bd65ca91a99b99462e37ee65c6da9ab584c6402
veoco <one@nomox.cn>
2022-03-21 15:43:03 +0800

    change conftest.py to debug mode

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,4 +6,9 @@ from pytypecho import Typecho
 
 @pytest.fixture(scope="module")
 def te():
-    return Typecho(os.environ.get('XMLRPC_URL'), username=os.environ.get('XMLRPC_USER_NAME'), password=os.environ.get('XMLRPC_USER_PASSWORD'))
+    return Typecho(
+        rpc_url=os.environ.get("XMLRPC_URL"),
+        username=os.environ.get("XMLRPC_USER_NAME"),
+        password=os.environ.get("XMLRPC_USER_PASSWORD"),
+        debug=True,
+    )
